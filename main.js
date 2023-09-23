2fc316b2f1bbcf6fe3897ca0bd54fff32d9f2447
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-09-23 14:05:10 +0430

    change theme based on user systems's preferred theme

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-const toggleTheme = document.querySelector(".theme-toggle")
-const body = document.querySelector("body")
+const toggleTheme = document.querySelector(".theme-toggle");
+const body = document.querySelector("body");
+
+const userPrefersDark =
+  window?.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches;
+const storedTheme = localStorage.getItem("theme");
+
+console.log(userPrefersDark);
+console.log(storedTheme);
+
+if (userPrefersDark) {
+  body.classList.add("dark-mode");
+}
 
 toggleTheme.addEventListener("click", () => {
-    body.classList.toggle("dark-mode")
-})
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
